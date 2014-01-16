adb88b560bef50203fe49cb01067f4f283f92d70
ken_goto <ken_goto@cybird.co.jp>
2014-01-16 19:28:26 +0900

    チケットの表示を変更テスト

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -147,20 +147,26 @@ module IssuesHelper
 
   def render_custom_fields_rows(issue)
     return if issue.custom_field_values.empty?
-    ordered_values = []
-    half = (issue.custom_field_values.size / 2.0).ceil
-    half.times do |i|
-      ordered_values << issue.custom_field_values[i]
-      ordered_values << issue.custom_field_values[i + half]
-    end
-    s = "<tr>\n"
-    n = 0
-    ordered_values.compact.each do |value|
-      s << "</tr>\n<tr>\n" if n > 0 && (n % 2) == 0
+    #ordered_values = []
+    #half = (issue.custom_field_values.size / 2.0).ceil
+    #half.times do |i|
+    #  ordered_values << issue.custom_field_values[i]
+    #  ordered_values << issue.custom_field_values[i + half]
+    #end
+    #s = "<tr>\n"
+    #n = 0
+    #ordered_values.compact.each do |value|
+    #  s << "</tr>\n<tr>\n" if n > 0 && (n % 2) == 0
+    #  s << "\t<th>#{ h(value.custom_field.name) }:</th><td>#{ simple_format_without_paragraph(h(show_value(value))) }</td>\n"
+    #  n += 1
+    #end
+    #s << "</tr>\n"
+    s = ""
+    issue.custom_field_values.compact.each do |value|
+      s << "<tr>\n"
       s << "\t<th>#{ h(value.custom_field.name) }:</th><td>#{ simple_format_without_paragraph(h(show_value(value))) }</td>\n"
-      n += 1
+      s << "</tr>\n"
     end
-    s << "</tr>\n"
     s.html_safe
   end
 
